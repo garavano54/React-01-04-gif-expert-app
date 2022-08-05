965bf7a9591a00508fb66e2decf90e68340af9ff
Alejandro Garavano Viotti <garavano54@gmail.com>
2022-08-05 01:48:03 -0300

    Primer Commit

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { GifExpertApp } from './GifExpertApp';
+
+import './styles.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GifExpertApp />
+  </React.StrictMode>
+);
+
+
+// Yarn build        (Para hacer el build) - (https://www.netlify.com/) - Para hacer el deploy
